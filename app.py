d16b98d537c628bbcf9cd507665c93e76c46be56
Hemanth Kumar Arikatla <hemanth.chowdary1314@gmail.com>
2020-09-23 04:23:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ app = Flask(__name__) # to initiate an instance of Flask app
 def welcome():
     return render_template('welcome.html')
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT'))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
+
+'''if __name__ == '__main__':
+    app.run(debug=True)'''
